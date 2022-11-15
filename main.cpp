799cc8848a9c54a3af9cfd9510d53760b70e5d0e
louiml <99025570+funmmer@users.noreply.github.com>
2022-11-15 08:13:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include "mainwindow.h"
-
 #include <QApplication>
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     QApplication app(argc, argv);
     MainWindow window;
     window.resize(640, 512);
